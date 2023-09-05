5f332977d7f4fcb70ba6d1aa5effcbef47d256b3
mridul-outscal <mridul.agrawal@outscal.com>
2023-09-05 18:17:34 +0530

    Added Graphic Service

---
+++ Array-Minesweeper/GraphicService.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class GraphicService
+{
+private:
+	// Constants
+	const std::string game_window_title = "Outscal Presents - Minesweeper";
+	const int game_window_width = 1920;
+	const int game_window_height = 1080;
+	const sf::Color window_color = sf::Color(200, 200, 0, 255);
+
+	sf::VideoMode video_mode;
+	sf::RenderWindow* game_window;
+
+	void configureVideoMode();
+
+public:
+	GraphicService();
+	~GraphicService();
+
+	void createGameWindow();
+	void updateGraphics();
+	void drawNextFrame();
+	bool isGameWindowOpen();
+	sf::RenderWindow* getWindow();
+	void setFrameRate(int);
+};
