9a59e81617511f93a99ac5e61ce2e1c80f9cde8d
mridul-outscal <mridul.agrawal@outscal.com>
2023-09-05 18:20:54 +0530

    Created Service Locator

---
+++ Array-Minesweeper/ServiceLocator.cpp
@@ -0,0 +1,49 @@
+#include "ServiceLocator.h"
+
+ServiceLocator::ServiceLocator() { initializeServices(); }
+
+ServiceLocator::~ServiceLocator() { clearAllServices(); }
+
+void ServiceLocator::initializeServices()
+{
+	event_service = new EventService();
+	graphic_service = new GraphicService();
+	ui_service = new UIService();
+}
+
+void ServiceLocator::clearAllServices()
+{
+	delete(event_service);
+	delete(graphic_service);
+	delete(ui_service);
+}
+
+ServiceLocator* ServiceLocator::getInstance()
+{
+	static ServiceLocator instance;
+	return &instance;
+}
+
+void ServiceLocator::setGameWindow()
+{
+	// Fetch Game Window from Graphic Service
+	game_window = graphic_service->getWindow();
+
+	// Pass down the reference of Game Window to other Services.
+	event_service->setGameWindow(game_window);
+	ui_service->setGameWindow(game_window);
+}
+
+void ServiceLocator::updateServices()
+{
+	graphic_service->updateGraphics();
+	ui_service->updateUI();
+}
+
+EventService* ServiceLocator::getEventService() { return event_service; }
+
+GraphicService* ServiceLocator::getGraphicService() { return graphic_service; }
+
+UIService* ServiceLocator::getUIService() { return ui_service; }
+
+void ServiceLocator::deleteServiceLocator() { delete(this); }
