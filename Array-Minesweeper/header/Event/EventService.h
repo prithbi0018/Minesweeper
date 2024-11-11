d9e59d8dbbd397eb2b99b56cd642203392bfd004
aryan-saxena-outscal <aryan.saxena@outscal.com>
2024-11-11 12:48:43 +0530

    Updated EventService for Mouse Button

--- Array-Minesweeper/header/Event/EventService.h
+++ Array-Minesweeper/header/Event/EventService.h
@@ -24,7 +24,7 @@ namespace Event
 		bool gameWindowWasClosed();
 		bool hasQuitGame();
 		bool isKeyboardEvent();
-		void updateButtonsState(ButtonState& button_state);
+		void updateButtonsState(ButtonState& button_state, sf::Mouse::Button button_type);
 
 	public:
 		EventService();
